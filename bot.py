01f1ede9d2ed932ea53a40cbd1200a5e0b1c8d8c
dimispringhetti <dimi.springhetti@gmail.com>
2024-11-08 20:39:19 +0100

    gestione database: fix richieste multiple e aggiunta di utenti approvati e non approvati

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@ from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
 import logging
 from dotenv import load_dotenv
 import os
-from database import add_user
+from database import add_user, is_user_not_approved, is_user_present, add_not_approved
 
 # Carica le variabili dal file .env
 load_dotenv()
@@ -26,6 +26,16 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     user = update.message.from_user
     chat_id = update.effective_chat.id
 
+    # fix richieste multiple:
+    # Se l'utente è già presente tra i non approvati, invia un messaggio di rifiuto
+    if is_user_not_approved(chat_id):
+        await context.bot.send_message(chat_id=chat_id, text=reject)
+        return
+    # Se l'utente è già presente tra gli utenti, invia un messaggio di benvenuto
+    if is_user_present(chat_id):
+        await context.bot.send_message(chat_id=chat_id, text=still_present)
+        return
+
     # Messaggio di benvenuto all'utente
     message = f"Ciao {user.first_name}, benvenuto in DollyMitraMovies.\nAttendi che gli amministratori attivino il servizio per te."
     await context.bot.send_message(chat_id=chat_id, text=message)
@@ -36,7 +46,7 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
 
 # Funzione per inviare un messaggio all'amministratore
 async def send_message_to_admin(context: ContextTypes.DEFAULT_TYPE, user, chat_id) -> None:
-    message = f"Nuovo utente ha avviato il bot: {user.first_name} (@{user.username}, ID: {user.id})"
+    message = f"Nuovo utente ha avviato il bot: {user.first_name} (@{user.username}, ID: {chat_id})"
 
     # Tastiera con approvazione o rifiuto, includendo l'ID della chat dell'utente nella callback_data
     keyboard = [
@@ -62,10 +72,7 @@ async def button_callback(update: Update, context: ContextTypes.DEFAULT_TYPE) ->
     # Gestione della risposta in base al pulsante premuto
     if action == 'approve':
         # Aggiungi l'utente alla lista degli utenti
-        if  not await add_user(query.from_user.id, query.from_user.username, query.from_user.first_name): # Se l'utente è già presente
-            await query.edit_message_text(text="Utente già presente!")
-            await context.bot.send_message(chat_id=int(chat_id), text=still_present)
-            return
+        await add_user(int(chat_id), query.from_user.username, query.from_user.first_name)
         # Invio di messaggio di benvenuto all'utente
         await context.bot.send_message(chat_id=int(chat_id), text=intro)
         # Invio di messaggio di approvazione all'admin
@@ -73,11 +80,12 @@ async def button_callback(update: Update, context: ContextTypes.DEFAULT_TYPE) ->
 
         
     elif action == 'reject':
+        # Aggiungi l'utente alla lista degli utenti non approvati
+        await add_not_approved(int(chat_id), query.from_user.username, query.from_user.first_name)
         # Invio di messaggio di rifiuto all'utente
         await context.bot.send_message(chat_id=int(chat_id), text=reject)
         # Invio di messaggio di rifiuto all'admin
         await query.edit_message_text(text="Utente rifiutato!")
-
 
 
 def main() -> None:
--- database.py
+++ database.py
@@ -8,12 +8,24 @@ def load_users():
     except FileNotFoundError:
         return []  # lista vuota se il file non esiste
 
+def load_not_approved():
+    try:
+        with open("not_approved.json", "r") as file:
+            return json.load(file)
+    except FileNotFoundError:
+        return []  # lista vuota se il file non esiste
+
+# Salva utenti non approvati nel file JSON
+def save_not_approved(not_approved):
+    with open("not_approved.json", "w") as file:
+        json.dump(not_approved, file, indent=4)
+
 # Salva utenti nel file JSON
 def save_users(users):
     with open("users.json", "w") as file:
         json.dump(users, file, indent=4)
 
-# ìControllo se l'utente è già presente
+# Controllo se l'utente è già presente
 def is_user_present(user_id):
     users = load_users()
     return any(user['ID'] == user_id for user in users)
@@ -21,14 +33,30 @@ def is_user_present(user_id):
 # Funzione per aggiungere un utente
 async def add_user(user_id, username, first_name):
     users = load_users()
-    # Controlla se l'utente è già presente
-    if is_user_present(user_id):
-        return False
-    else:
-        users.append({
-            "ID": user_id,
-            "USERNAME": username,
-            "FIRST_NAME": first_name
-        })
-        save_users(users)
-        return True
+    users.append({
+        "ID": user_id,
+        "USERNAME": username,
+        "FIRST_NAME": first_name
+    })
+    save_users(users)
+
+# Funzione per aggiungere un utente non approvato
+async def add_not_approved(user_id, username, first_name):
+    not_approved = load_not_approved()
+    not_approved.append({
+        "ID": user_id,
+        "USERNAME": username,
+        "FIRST_NAME": first_name
+    })
+    save_not_approved(not_approved)
+
+
+# Funzione per controllare se l'utente è già tra i non approvati
+def is_user_not_approved(user_id):
+    not_approved = load_not_approved()
+    return any(user['ID'] == user_id for user in not_approved)
+
+# Funzione per controllare se l'utente è già tra gli users
+def is_user_present(user_id):
+    users = load_users()
+    return any(user['ID'] == user_id for user in users)
